308baa70bd5f9661884e0c323bbfb54ccc55f1a1
Freddy Hidalgo-Monchez <freddy.hm@growple.com>
2015-01-08 22:38:44 -0800

    tweak scripts

--- test.php
+++ test.php
@@ -4,11 +4,12 @@ $module = $_POST["module"];
 
 echo $module;
 
-exec('osascript  close_all_chrome_tabs.scpt');
+exec('osascript close_all_chrome_tabs.scpt');
 
 if($module == "start"){
 	exec('osascript -e \'tell app "iTunes" to play the playlist "Morning"\'');	
 }else if($module == "code"){
+	exec('osascript close_all_apps.scpt');
 	exec('osascript -e \'tell app "iTunes" to play the playlist "Music"\'');
 	exec('osascript -e \'open app "Xcode"\'');
 	exec('osascript -e \'open app "Slack"\'');
@@ -18,17 +19,20 @@ if($module == "start"){
 	exec('osascript close_all_apps.scpt');
 	//exec('osascript -e \'open location "https://www.youtube.com/watch?v=1dWjKkF0Zi4"\'');
 }else if($module == "write"){
+	exec('osascript close_all_apps.scpt');
 	exec('osascript -e \'tell app "iTunes" to play the playlist "Classical Music"\'');	
 	exec('osascript -e \'open app "Sublime Text"\'');
 }else if($module == "ukulele"){
-	exec('osascript -e \'tell application "iTunes" to quit\'');	
+	exec('osascript close_all_apps.scpt');
 	exec('osascript -e \'open location "http://analogmusicstudios.com/sublime-santeria-ukulele-chords/"\'');
 }else if($module == "backtest"){
+	exec('osascript close_all_apps.scpt');
 	exec('osascript -e \'tell application "Finder" to open POSIX file "/Users/fhm/VirtualBox VMs/Windows/Windows.vbox"\'');
 	exec('osascript -e \'tell application "Finder" to open POSIX file "/Users/fhm/Dropbox/Personal/Trading/Live Account copy.xlsx"\'');
 	exec('osascript -e \'open location "http://www.forexfactory.com"\'');
 	exec('osascript -e \'tell app "iTunes" to play the playlist "Music"\'');
 }else if($module == "analytics"){
+	exec('osascript close_all_apps.scpt');
 	exec('osascript -e \'tell app "iTunes" to play the playlist "Music"\'');
 	exec('osascript -e \'open location "https://mixpanel.com/report/477109/events/#events"\'');
 	exec('osascript -e \'open location "https://www.google.com/analytics/web/?hl=en#report/app-overview/a49381420w81115025p83961631/"\'');
