8f6655e6f48592a36d349edf8fdb4998767ff576
Freddy Hidalgo-Monchez <freddy.hm@growple.com>
2015-01-15 19:50:07 -0800

    Add shuffle to playlist, get current time, and screen dim/bright when leaving/coming back

--- test.php
+++ test.php
@@ -5,24 +5,29 @@ $module = $_POST["module"];
 echo $module;
 
 if($module == "start"){
-	exec('osascript -e \'tell app "iTunes" to play the playlist "Morning"\'');	
+	exec('osascript bright_screen.scpt');	
+	exec('osascript -e \'tell application "System Events" to tell process "iTunes" to click menu item 1 of menu 1 of menu item "Shuffle" of menu 1 of menu bar item "Controls" of menu bar 1\'');
+	exec('osascript -e \'tell app "iTunes" to play the playlist "Morning"\'');
 }else if($module == "code"){
 	exec('osascript close_all_chrome_tabs.scpt');
 	exec('osascript close_all_apps.scpt');
 	exec('osascript -e \'tell app "iTunes" to play the playlist "Music"\'');
+	exec('osascript -e \'tell application "System Events" to tell process "iTunes" to click menu item 1 of menu 1 of menu item "Shuffle" of menu 1 of menu bar item "Controls" of menu bar 1\'');
 	exec('osascript -e \'tell application "Xcode" to activate\'');
 	exec('osascript -e \'tell application "Slack" to activate\'');
 	exec('osascript -e \'open location "https://app.asana.com/"\'');
-	exec('osascript -e \'open location "http://www.konotor.com"\'');
+	exec('osascript -e \'open location "http://app.konotor.com/app/home#/"\'');
 	exec('osascript -e \'tell application "System Events" to set frontmost of process "Slack" to true\'');
 } else if($module == "stop"){
 	exec('osascript close_all_chrome_tabs.scpt');
 	exec('osascript -e \'tell app "iTunes" to quit\'');
 	exec('osascript close_all_apps.scpt');
+	exec('osascript dim_screen.scpt');
 }else if($module == "write"){
 	exec('osascript close_all_chrome_tabs.scpt');
 	exec('osascript close_all_apps.scpt');
 	exec('osascript -e \'tell app "iTunes" to play the playlist "Classical Music"\'');	
+	exec('osascript -e \'tell application "System Events" to tell process "iTunes" to click menu item 1 of menu 1 of menu item "Shuffle" of menu 1 of menu bar item "Controls" of menu bar 1\'');
 	exec('osascript -e \'tell application "Sublime Text" to activate\'');
 	exec('osascript -e \'open location "https://app.asana.com/0/23952007863201/23952007863201/"\'');
 	exec('osascript -e \'tell application "System Events" to set frontmost of process "Sublime Text" to true\'');
@@ -40,12 +45,15 @@ if($module == "start"){
 	exec('osascript -e \'tell application "Finder" to open POSIX file "/Users/fhm/Dropbox/Personal/Trading/Live Account copy.xlsx"\'');
 	exec('osascript -e \'open location "http://www.forexfactory.com"\'');
 	exec('osascript -e \'tell app "iTunes" to play the playlist "Music"\'');
+	exec('osascript -e \'tell application "System Events" to tell process "iTunes" to click menu item 1 of menu 1 of menu item "Shuffle" of menu 1 of menu bar item "Controls" of menu bar 1\'');
 	exec('osascript -e \'open location "https://app.asana.com/0/23952007863199/23952007863199"\'');
 	exec('osascript -e \'tell application "System Events" to set frontmost of process "Virtual Box" to true\'');
 }else if($module == "analytics"){
 	exec('osascript close_all_chrome_tabs.scpt');
 	exec('osascript close_all_apps.scpt');
 	exec('osascript -e \'tell app "iTunes" to play the playlist "Music"\'');
+	exec('osascript -e \'tell application "System Events" to tell process "iTunes" to click menu item 1 of menu 1 of menu item "Shuffle" of menu 1 of menu bar item "Controls" of menu bar 1\'');
+	exec('osascript -e \'tell application "Slack" to activate\'');
 	exec('osascript -e \'open location "https://mixpanel.com/report/477109/events/#events"\'');
 	exec('osascript -e \'open location "https://www.google.com/analytics/web/?hl=en#report/app-overview/a49381420w81115025p83961631/"\'');
 	exec('osascript -e \'open location "https://crashlytics.com/teamstory/ios/apps/com.growple.teamstory/answers/daily_active_users"\'');
@@ -53,6 +61,7 @@ if($module == "start"){
 	exec('osascript -e \'tell application "System Events" to set frontmost of process "Google Chrome" to true\'');
 }else if($module == "music"){
 	exec('osascript -e \'tell app "iTunes" to play the playlist "Music"\'');
+	exec('osascript -e \'tell application "System Events" to tell process "iTunes" to click menu item 1 of menu 1 of menu item "Shuffle" of menu 1 of menu bar item "Controls" of menu bar 1\'');
 }else if($module == "next"){
 	exec('osascript -e \'tell app "iTunes" to play next track\'');
 }else if($module == "stop_music"){
@@ -61,9 +70,14 @@ if($module == "start"){
 	exec('osascript close_all_chrome_tabs.scpt');
 	exec('osascript close_all_apps.scpt');
 	exec('osascript -e \'tell app "iTunes" to play the playlist "Music"\'');
+	exec('osascript -e \'tell application "System Events" to tell process "iTunes" to click menu item 1 of menu 1 of menu item "Shuffle" of menu 1 of menu bar item "Controls" of menu bar 1\'');
 	exec('osascript -e \'tell application "Sublime Text" to activate\'');
 	exec('osascript -e \'open location "https://app.asana.com/0/23952007863213/23952007863213"\'');
 	exec('osascript -e \'tell application "System Events" to set frontmost of process "Sublime Text" to true\'');
+}else if($module == "off"){
+	exec('osascript dim_screen.scpt');
+}else if($module == "back"){
+	exec('osascript bright_screen.scpt');
 }
 
 
